3b6fcef56ccba0c889e9fd36387ae254cf38c12c
thaisrocine_ <69332861+thaisrocine@users.noreply.github.com>
2024-08-15 19:14:01 -0300

    added icon button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,20 +9,20 @@ const Header = () => {
   const pathname = usePathname();
 
   const getTitle = () => {
-
     return <img src="/img/logotipo.png" alt="Logo" className={styles.logo} />;
-   
   };
 
   return (
     <header className={styles.header}>
-      <Link href="/">
-        <button className={styles.backButton}>
-          &#x2190;
-        </button>
-      </Link>
+      {pathname !== "/" && (
+        <Link href="/">
+          <button className={styles.backButton}>
+            <img src="/img/seta-esq.png" alt="Back" className={styles.backIcon} />
+          </button>
+        </Link>
+      )}
       <h1 className={styles.title}>{getTitle()}</h1>
-      <Profile/>
+      <Profile />
     </header>
   );
 };
