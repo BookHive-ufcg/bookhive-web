0b185e2809a900ed9fb3c8b753ce7f3810040c93
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2024-09-06 16:55:24 -0300

    color to global and define size

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -3,11 +3,15 @@ import styles from "./profile.module.css";
 
 type SearchBarProps = {
   placeholder?: string;
+  width?: number;
 };
 
-export default function SearchBar({ placeholder }: SearchBarProps) {
+export default function SearchBar({
+  placeholder,
+  width = 600,
+}: SearchBarProps) {
   return (
-    <div className={styles.searchBar}>
+    <div style={{ width }} className={styles.searchBar}>
       <FiSearch className={styles.icon} />
       <input type="text" placeholder={placeholder} className={styles.input} />
     </div>
