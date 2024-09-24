d7366de22bb3d9c239a8c98ba9951fea92b56e2a
Maria <maria.marinho@ccc.ufcg.edu.br>
2024-09-24 16:14:01 -0300

    add title

--- src/app/reviews/create/Coments/index.tsx
+++ src/app/reviews/create/Coments/index.tsx
@@ -4,6 +4,14 @@ import styles from "./coments.module.css";
 const Coments = () => {
     return(
     <div className={styles.comentsContainer}>
+        <div className={styles.tituloFieldLabel}>
+            <label>Title</label>
+            <input
+            required
+            type='text'
+            className={styles.tituloFieldInput}>
+            </input>
+        </div>
         <div className={styles.comentsFieldLabel}>
             <label>What did you think about the book?</label>
             <input
