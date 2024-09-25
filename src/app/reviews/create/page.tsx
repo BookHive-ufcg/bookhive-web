90564ea63678c46859d33904f02a8e68687f59af
Maria <maria.marinho@ccc.ufcg.edu.br>
2024-09-24 21:49:48 -0300

    botao de submit

--- src/app/reviews/create/page.tsx
+++ src/app/reviews/create/page.tsx
@@ -4,13 +4,17 @@ import Title from "@/components/Title";
 import DateFields from "./DateFields";
 import Coments from "./Coments";
 import Animation from "./Animation";
+import styles from "./page.module.css";
 
 export default function CreateReview() {
   return <main>
-        <Title titleText="Review" subTitleText="Create the best review" />
+    <Title titleText="Review" subTitleText="Create the best review" />
+    <form>
         <DateFields></DateFields>
         <Animation></Animation>
         <Coments></Coments>
+        <input type='submit' value='Submit review' className={styles.submit}></input>
+    </form>
         <h1>teste</h1>
   </main>;
 }
