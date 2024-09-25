8379f66b503738eeaa84c10507bd6c0ecab4b8ed
thaisrocine_ <69332861+thaisrocine@users.noreply.github.com>
2024-09-25 19:06:22 -0300

    teste deploy

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const Header = () => {
+  
   const pathname = usePathname();
 
   const router = useRouter() ?? null;
