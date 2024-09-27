915834a915531b3d0055dab698e318e6b2d30aa7
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2024-09-27 10:24:17 -0300

    fixing homepage width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation'; // Certifique-se de estar usando o 'useRouter' correto
-import { useState } from 'react';
+import { useRouter } from "next/navigation"; // Certifique-se de estar usando o 'useRouter' correto
+import { useState } from "react";
 import googleBooksService from "@/services/googleBooksService";
-import Title from '@/components/Title';
-import SearchBar from '@/components/SearchBar';
-import styles from './search.module.css'
-import Animation from './reviews/create/Animation';
-
+import Title from "@/components/Title";
+import SearchBar from "@/components/SearchBar";
+import styles from "./search.module.css";
+import Animation from "./reviews/create/Animation";
 
 export default function Home() {
   const router = useRouter();
@@ -28,10 +27,10 @@ export default function Home() {
       const book = await googleBooksService.getBookById(id);
 
       // Armazena os dados do livro no localStorage (opcional, pode usar context ou outros métodos)
-      localStorage.setItem('selectedBook', JSON.stringify(book));
+      localStorage.setItem("selectedBook", JSON.stringify(book));
 
       // Redireciona para a página de reviews/view
-      router.push('/reviews/view');
+      router.push("/reviews/view");
     } catch (error) {
       console.error("Error fetching book details:", error);
     }
@@ -39,49 +38,50 @@ export default function Home() {
 
   return (
     <main>
-      <Title
-        titleText="Welcome to Bookhive!"
-        subTitleText={"Discover more about book's world"}
-      />
-
-      <div className={styles.searchBarContainer}>
-        <SearchBar
-          placeholder="Title, author or ISBN"
-          onSearch={handleSearch}
+      <div className={styles.container}>
+        <Title
+          titleText="Welcome to Bookhive!"
+          subTitleText={"Discover more about book's world"}
         />
 
-        {searchResults.length > 0 && (
-          <div className={styles.searchResults}>
+        <div className={styles.searchBarContainer}>
+          <SearchBar
+            placeholder="Title, author or ISBN"
+            onSearch={handleSearch}
+          />
 
-            <h2 className={styles.searchResultsTitle}>Search Results</h2>
-            <div className={styles.bookGrid}>
-              {searchResults.map((book: any) => (
-                <div
-                  key={book.id}
-                  className={styles.bookCard}
-                  onClick={() => handleChange(book.id)} // Chamando handleChange com o ID do livro
-                >
-                  <img
-                    src={
-                      book.volumeInfo.imageLinks?.thumbnail ||
-                      "/img/book-placeholder.png"
-                    }
-                    alt={book.volumeInfo.title}
-                    className={styles.bookCover}
-                  />
-                  <div className={styles.bookInfo}>
-                    <h3 className={styles.bookTitle}>
-                      {book.volumeInfo.title}
-                    </h3>
-                    <p className={styles.bookAuthor}>
-                      {book.volumeInfo.authors?.join(", ")}
-                    </p>
+          {searchResults.length > 0 && (
+            <div className={styles.searchResults}>
+              <h2 className={styles.searchResultsTitle}>Search Results</h2>
+              <div className={styles.bookGrid}>
+                {searchResults.map((book: any) => (
+                  <div
+                    key={book.id}
+                    className={styles.bookCard}
+                    onClick={() => handleChange(book.id)} // Chamando handleChange com o ID do livro
+                  >
+                    <img
+                      src={
+                        book.volumeInfo.imageLinks?.thumbnail ||
+                        "/img/book-placeholder.png"
+                      }
+                      alt={book.volumeInfo.title}
+                      className={styles.bookCover}
+                    />
+                    <div className={styles.bookInfo}>
+                      <h3 className={styles.bookTitle}>
+                        {book.volumeInfo.title}
+                      </h3>
+                      <p className={styles.bookAuthor}>
+                        {book.volumeInfo.authors?.join(", ")}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </main>
   );
